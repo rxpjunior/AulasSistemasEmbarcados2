c523489244052018a7c66fd80b6e0b289b6a471d
rxpjunior <rxpjunior@gmail.com>
2022-10-22 18:44:21 -0300

    Subindo projeto de irrigação primeira versao

--- SistemaIrrigacao/Teste1/Teste1.ino
+++ SistemaIrrigacao/Teste1/Teste1.ino
@@ -1,14 +1,13 @@
+//Teste de higrometro
+
 int PinoAnalogico = A0; // Define o pino A0 como Pino Analógico do sensor
-int EstadoSensor = 0;
-int UltimoEstSensor = 0;
 int ValAnalogIn; // Valor analógico no código
 void setup() {
 Serial.begin(115200);
 }
 void loop() {
 ValAnalogIn = analogRead(PinoAnalogico);
-int Porcento = map(ValAnalogIn, 200, 260, 0, 100); // Traforma o valor analógico em porcentagem
 Serial.print("Leitura: "); // Imprime o símbolo no valor
-Serial.println(Porcento); // Imprime o valor em Porcentagem no monitor Serial
-delay(2000);
+Serial.println(ValAnalogIn); // Imprime o valor em Porcentagem no monitor Serial
+delay(3000);
 }
